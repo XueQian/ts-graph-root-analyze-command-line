e11b6f9a56d049efda28b08467219834f0ebbf01
XUE Qian <xueqian0321@outlook.com>
2019-11-25 14:42:00 +0800

    get the procedures from mysql

--- src/database/my-sql-client.ts
+++ src/database/my-sql-client.ts
@@ -1,38 +1,42 @@
-import { createPool, Pool } from "mysql2/promise";
-import { DbClient } from "./DbClient";
+import {createPool, Pool} from "mysql2/promise";
+import {DbClient} from "./db-client";
 
 class MySql implements DbClient {
-  private pool: Pool;
+    private pool: Pool;
 
-  public constructor(host: string, user: string, password: string, database: string, connectionLimit = 5) {
-    this.pool = createPool({
-      host,
-      database,
-      user,
-      password,
-      connectionLimit
-    });
-  }
-
-  public async query(sql: string): Promise<any> {
-    console.log(`Query mysql: ${sql}`);
-    const connection = await this.pool.getConnection();
-    try {
-      const [items] = await connection.query(sql);
-      return items;
-    } catch (e) {
-      console.error(`Error happened when execute the query to mysql: ${e}`);
-      throw e;
-    } finally {
-      console.log(`release current db connection...`);
-      await connection.release();
+    public constructor(host: string, port: number, user: string, password: string, database: string,
+                       connectionLimit = 5, acquireTimeout = 60000) {
+        this.pool = createPool({
+            host,
+            port,
+            database,
+            user,
+            password,
+            connectionLimit,
+            acquireTimeout
+        });
     }
-  }
 
-  public async close() {
-    console.log("close mysql pool...");
-    await this.pool.end();
-  }
+    public async query(sql: string): Promise<any> {
+        console.log(`Query mysql: ${sql}`);
+        const connection = await this.pool.getConnection();
+
+        try {
+            const [items] = await connection.query(sql);
+            return items;
+        } catch (e) {
+            console.error(`Error happened when execute the query to mysql: ${e}`);
+            throw e;
+        } finally {
+            console.log(`release current db connection...`);
+            await connection.release();
+        }
+    }
+
+    public async close() {
+        console.log("close mysql pool...");
+        await this.pool.end();
+    }
 }
 
-export { MySql };
+export {MySql};
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
 #!/usr/bin/env ts-node
 
 import * as program from "commander";
-import {exportPlsqlRoot} from "./exporters/export-plsql-root";
-import {getClass, getMethod, PlsqlName} from "./providers/deps";
+import {getClass, getMethod} from "./providers/deps";
 import {PlSqlRoot, PlsqlUseCaseExternPointAnalyzor} from "./services/analyze-plsql-use-case-extern-point";
+import {MySql} from "./database/my-sql-client";
+import {DbClient} from "./database/db-client";
 
 program.command("analyze")
     .option("-l, --url <dependency service root>", "dependency service root")
     .option("-v, --visual <dependency visual root>", "dependency visual root")
     .action(async (options: AnalyzeOption) => {
         const service = new PlsqlUseCaseExternPointAnalyzor();
-        const plsqls = [];
-        const ignoreJavaMethodNames = [''];
-        const roots = await service.justDo(options.url, plsqls, ignoreJavaMethodNames);
-        const result = exportPlsqlRoot(attachToUrl(options.visual, roots));
+        // const plsqls = [{"pkg": "PKG_LIFE_NEWBIZ_APPRO_INC", "method": "P_APPROVAL_INSERT_REAL_FEE"}];
 
-        console.log(result);
+        // const dbClient: DbClient = new MySql( "10.127.151.14", 8306, "root", "prisma", "default@default");
+        const dbClient: DbClient = new MySql("localhost", 3306, "root", "", "test");
+        let result = await dbClient.query("select * from POLICY_FEE_PROCEDURE;");
+
+        result.map((item: any) => {
+            console.log(item.clz);
+        });
+
+        await dbClient.close();
+        // console.log(procedureEntities);
+
+        // const plsqls = [];
+        // const ignoreJavaMethodNames = [''];
+        // const roots = await service.justDo(options.url, plsqls, ignoreJavaMethodNames);
+        // const result = exportPlsqlRoot(attachToUrl(options.visual, roots));
+        //
+        // console.log(result);
     });
 
 program.parse(process.argv);
